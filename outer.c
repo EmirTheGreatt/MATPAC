7f429150358c08e10ca7fc8b07cc3f9a2be8b5a8
Emir the Great <emirergun@live.com>
2026-03-19 18:21:59 +0300

    outer.c

--- outer.c
+++ outer.c
@@ -7,5 +7,10 @@ Matrix  *outer(Matrix *M1, Matrix *M2)
 		return NULL;
 	}
 	Matrix *to_be_returned = new_mat(M1 -> rows, M2 -> rows);
-	
+	for (size_t row = 0; row < M1 -> rows; row++)
+	for (size_t col = 0; col < M2 -> rows; col++)
+		set_coeff(to_be_returned, row, col,
+			get_coeff(M1, row, 0) * get_coeff(M2, col, 0));
+	return to_be_returned;
+
 }
